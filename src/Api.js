e609ff953dcc06cf1d171bc4ec6d93cb0e1ebe05
Ilya Lebedev <bacherko@gmail.com>
2019-07-17 14:24:19 +0300

    feat: separate main and api processes

--- src/Api.js
+++ src/Api.js
@@ -1,17 +1,26 @@
 const core = require('gls-core-service');
 const BasicMain = core.services.BasicMain;
 const env = require('./data/env');
-const Subscriber = require('./services/Subscriber');
+const Blocks = require('./controllers/Blocks');
+const Graphs = require('./controllers/Graphs');
+const Connector = require('./services/Connector');
 const ServiceMetaModel = require('./models/ServiceMeta');
 
-class Main extends BasicMain {
+class ApiMain extends BasicMain {
     constructor() {
         super(env);
 
         this.startMongoBeforeBoot();
 
-        this._subscriber = new Subscriber();
-        this.addNested(this._subscriber);
+        this._blocks = new Blocks();
+        this._graphs = new Graphs();
+
+        this._connector = new Connector({
+            blocks: this._blocks,
+            graphs: this._graphs,
+        });
+
+        this.addNested(this._connector);
     }
 
     async boot() {
@@ -28,4 +37,4 @@ class Main extends BasicMain {
     }
 }
 
-module.exports = Main;
+module.exports = ApiMain;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-require('gls-core-service').utils.defaultStarter(require('./Main'));
+const gls = require('gls-core-service');
+
+if (process.env.GLS_API) {
+    gls.utils.defaultStarter(require('./Api'));
+} else {
+    gls.utils.defaultStarter(require('./Main'));
+}
